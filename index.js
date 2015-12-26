74da641d7dbc3898e324ebd933f2e07fc4d115eb
gumbyferret <yb@converged-systems.com>
2015-12-26 19:27:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var gpio = require("pi-gpio");
+
+//var intervalId;
+//var durationId;
+//var Pin1 = 7;
+//var Pin2 = 12;
+//var Pin = 7;
 
 app.get('/', function(req, res) {
   //res.send('<h1>Hello World</h1>');  //send Hello World
   res.sendFile(__dirname + '/index.html'); //send a file
 });
 
-io.on('connection', function(socket) {
-  //console.log('a user connected');
-  //socket.on('disconnect', function(){
-  //  console.log('user disconnected');
-  //})
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg); //broadcast message to everyone
+gpio.open(Pin, "output", function(err) {
+	console.log('GPIO pin '+Pin+' is open');
+  	gpio.write(Pin, 1, function() { // toggle pin between high (1) and low (0)
+  	});
+});
+
+gpio.close(Pin, "output", function(err) {
+	console.log('GPIO pin '+Pin+' is off');
+  gpio.write(Pin, 0, function() { // turn off Pin
+    gpio.close(Pin); // then Close Pin
   });
 });
 
