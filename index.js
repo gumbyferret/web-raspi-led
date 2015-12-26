abe425ea964010c039b99dd2194822431c4ad9c3
gumbyferret <yb@converged-systems.com>
2015-12-26 21:19:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,43 @@ var gpio = require("pi-gpio");
 //var durationId;
 //var Pin1 = 7;
 //var Pin2 = 12;
-var Pin = 7;
+
 
 app.get('/', function(req, res) {
   //res.send('<h1>Hello World</h1>');  //send Hello World
   res.sendFile(__dirname + '/index.html'); //send a file
 });
 
-gpio.open(Pin, "output", function(err) {
-	console.log('GPIO pin '+Pin+' is open');
-  	gpio.write(Pin, 1, function() { // toggle pin between high (1) and low (0)
-  	});
+app.put('/OnRed', function(req, res) {
+	gpio.open(12, "output", function(err) {
+		console.log('GPIO pin 12 is open');
+  			gpio.write(12, 1, function() { // toggle pin between high (1) and low (0)
+  		});
+	});
 });
 
-gpio.close(Pin, function(err) {
-	console.log('GPIO pin '+Pin+' is off');
-  	gpio.write(Pin, 0, function() { // turn off Pin
-  	});
+app.put('/OffRed', function(req, res) {
+	console.log('GPIO pin 12 is off');
+	gpio.write(12, 0, function() { // turn off Pin
+	});
+	gpio.close(12, function(err) {
+	});
+});
+
+app.put('/OnYellow', function(req, res) {
+	gpio.open(7, "output", function(err) {
+		console.log('GPIO pin 7 is open');
+  			gpio.write(7, 1, function() { // toggle pin between high (1) and low (0)
+  		});
+	});
+});
+
+app.put('/OffYellow', function(req, res) {
+	console.log('GPIO pin 7 is off');
+	gpio.write(7, 0, function() { // turn off Pin
+	});
+	gpio.close(7, function(err) {
+	});
 });
 
 http.listen(3000, function(){
